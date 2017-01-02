f4014c1beb17e24d84bbb47c78ac56e62e3274e0
spssciro <afsciro@spscommerce.com>
2017-01-02 12:56:14 -0500

    adding radius support for emitter class + making particles emit around circle diameter

--- _lib/emitter.js
+++ _lib/emitter.js
@@ -1,10 +1,14 @@
 class Emitter {
-    constructor(x, y, emitFlag, vx, vy) {
+    constructor(x, y, emitFlag, radius, vx, vy) {
         this.x = x;
         this.y = y;
         this.vx = vx;
         this.vy = vy;
         this.emitFlag = emitFlag;
+        //if radius set radius for emitter
+        if(radius){
+            this.radius = radius;
+        }
     }
 }
 
--- rainbow emitter/main-es6.js
+++ rainbow emitter/main-es6.js
@@ -9,8 +9,8 @@ this.vm = {}
 vm.stage = new createjs.Stage('canvas');
 vm.canvas = document.getElementById("canvas");
 vm.particleArray = [];
-vm.emitter = new Emitter(window.innerWidth/2, window.innerHeight/2,true);
-vm.particleAmt = 200;
+vm.emitter = new Emitter(window.innerWidth/2, window.innerHeight/2, true, 100);
+vm.particleAmt = 400;
 vm.increment = 0;
 vm.gravity = 0.04;
 
@@ -20,7 +20,7 @@ init();
 function init(){
 
     createjs.Ticker.addEventListener("tick", handleTick);
-    createjs.Ticker.setFPS(60);
+    createjs.Ticker.setFPS(120);
     canvas.width = jQuery(window).width();
     canvas.height = jQuery(window).height();
 
@@ -29,9 +29,12 @@ function init(){
 
 function createParticles(){
     if(vm.particleArray.length <= vm.particleAmt && vm.emitter.emitFlag === true){
-        for(var i = 0; i < 1; i++){            
-            var testParticle = new Particle(vm.emitter.x,vm.emitter.y,(Math.random() - 0.5) * 1.2,(Math.random() - 0.5) * 1.2, vm.stage, 30);
-            testParticle.createShape('circle', 3, "#"+((1<<24)*Math.random()|0).toString(16));
+        for(var i = 0; i < 1; i++){
+            vm.increment++;
+            console.log( vm.emitter)
+            Math.cos(vm.increment) * vm.emitter.radius;
+            var testParticle = new Particle((Math.cos(vm.increment) * vm.emitter.radius) + vm.emitter.x, (Math.sin(vm.increment) * vm.emitter.radius) + vm.emitter.y,(Math.random() - 0.5) * 1,(Math.random() - 0.5) * 1, vm.stage, 30);
+            testParticle.createShape(1, 4, "#"+((1<<24)*Math.random()|0).toString(16));
             vm.particleArray.push(testParticle);
         }
     }
@@ -45,10 +48,9 @@ function moveEmitterAroundStage(){
 
 
 function handleTick(){
-
     createParticles();
     vm.particleArray.forEach((pa,i) =>{
-        
+
         //pa.p.vy += vm.gravity;
         pa.p.vx *= 1.01;
         pa.p.vy *= 1.01;
