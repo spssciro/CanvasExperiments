9768e22671d9746087695331b0c07fdae94ddbb7
spssciro <afsciro@spscommerce.com>
2017-01-02 09:50:24 -0500

    adding emitter class + tweaking particle class

---
+++ _lib/emitter.js
@@ -0,0 +1,10 @@
+class Emitter {
+    constructor(x, y, vx, vy) {
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+    }
+}
+
+export default Emitter;
--- _lib/particle.js
+++ _lib/particle.js
@@ -1,12 +1,48 @@
 class Particle {
-    constructor(x, y, vx, vy) {
-        this.x = x;
-        this.y = y;
-        this.vx = yx;
-        this.vy = vy;
+    constructor(x, y, vx, vy, stageReference, life) {
+        this.p = new createjs.Shape();
+        this.p.x = x;
+        this.p.y = y;
+        this.p.vx = vx;
+        this.p.vy = vy;
+        this.life = life;
+        this.deadFlag = false;
+        
+        //If we pass in stage lets just add it
+        if(stageReference){
+            stageReference.addChild(this.p);
+        }
     }
-    move(){
-        this.x = this.x + this.vx;
-        this.y = this.y + this.vy;        
+    
+    createShape(type, size, color){
+        if(type === "square" || type === 1){
+            this.p.graphics.beginFill(color).drawRect(0, 0, size,size);
+        }
+        else{
+            this.p.graphics.beginFill(color).drawCircle(0, 0, size);
+        }
+    }
+    move(x,y){
+        this.p.x = x;
+        this.p.y = y; 
+    }
+    accelerate(vx,vy){
+        this.p.x = this.p.x + vx;
+        this.p.y = this.p.y + vy;          
+    }
+    lifeSpan(decrease, callback){
+        this.life -= decrease;
+        if(this.life <= 0){
+            this.deadFlag = true;
+        }
+    }
+    animate(){
+        this.p.x = this.p.x + this.p.vx;
+        this.p.y = this.p.y + this.p.vy;
+    }    
+    remove(stage){
+        stage.removeChild(this.p);
     }
 }
+
+export default Particle;
