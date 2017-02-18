588acd756e0d507fe1aa1fa7369a3ca46ff1aaac
spssciro <afsciro@spscommerce.com>
2017-02-18 10:03:04 -0500

    Tweaks to the physics tests

--- boring-tests/pixi-tests/main-es6.js
+++ boring-tests/pixi-tests/main-es6.js
@@ -10,77 +10,80 @@ function App(){
  
     // Init p2.js
     let world = new p2.World({gravity: [0,-9.8]});
-    world.defaultContactMaterial.friction = 1;
+    world.defaultContactMaterial.friction = 0;
 	world.defaultContactMaterial.restitution = .5;
 	world.defaultContactMaterial.stiffness = 5;
  
     // create a testCollisionGroup 
     var testCollisionGroup = 0x0001;
     let squareArray = [];
-	const particleAmt = 120;
+	const particleAmt = 100;
 	let inc = 0;
+    let slowInc = 0;
 
- 
-    for(var i = 0; squareArray.length < particleAmt; i++){
-		
-        var s = {};
-		let size = (Math.random() * 30) + 10;
-        // Add a square
-        s.squareShape2 = new p2.Box({ width: size, height: size});
-        s.squareBody2 = new p2.Body({ mass:1, position:[w/2,0], sleepTimeLimit: 1, sleepSpeedLimit:0.1 });
-        s.squareShape2.material = new p2.Material();
-		s.life = (Math.random() * 100) + 50;
-        s.square = new PIXI.Graphics();
-        s.square.lineStyle(0);
-        s.square.beginFill(0x32BE6F, .9);
-        s.square.drawRect(0, 0,size, size);
-        s.square.endFill();
-        s.square.pivot.set(size/2);
-        world.addBody(s.squareBody2);
-        s.squareBody2.addShape(s.squareShape2);
-        app.stage.addChild(s.square);
-        squareArray.push(s);
+    function createParticles(){
+        for(var i = 0; i < 2 && squareArray.length < particleAmt; i++){    
+            var s = {};
+            let size = (Math.random() * 10) + 10;
+            // Add a square
+            s.squareShape2 = new p2.Circle({ radius: size});
+            s.squareBody2 = new p2.Body({ mass:1, position:[w/2,0] });
+            s.squareShape2.material = new p2.Material();
+            s.life = (Math.random() * 200) + 200;
+            s.square = new PIXI.Graphics();
+            s.square.lineStyle(0);
+            s.square.beginFill(0x32BE6F, .9);
+            s.square.drawCircle(0,0, size);
+            s.square.endFill();
+            world.addBody(s.squareBody2);
+            s.squareBody2.addShape(s.squareShape2);
+            app.stage.addChild(s.square);
+            squareArray.push(s);
+        }
     }
-     
+    
     // Add a plane
     let planeShape = new p2.Plane();
-    let planeBody = new p2.Body({ position:[0, -h+100 ] });
+    let planeBody = new p2.Body({ position:[0, -h+100],angle:.075 });
     planeShape.material = new p2.Material();
     planeBody.addShape(planeShape); 
     world.addBody(planeBody);
  
     let plane = new PIXI.Graphics();
     plane.beginFill(0x0097cc);
-    plane.drawRect(0, 0, w, 200);
+    plane.drawRect(0, 0, w, 300);
     plane.endFill();
     plane.x = planeBody.position[0];
     plane.y = planeBody.position[1] *-1;
+    plane.rotation = -planeBody.angle;    
 
     //Add to stage
     app.stage.addChild(plane);  
  
     app.ticker.add((delta) => {
  
-	    inc += .2;
+	    inc += .125;
+        
+
+        createParticles();
 
         for(var o = 0; o < squareArray.length; o++){
             var ls = squareArray[o];
 			ls.life -= 1;
             ls.square.x = ls.squareBody2.position[0];
             ls.square.y = (ls.squareBody2.position[1] *-1);
-            ls.square.rotation = ls.squareBody2.angle;
+            ls.square.rotation = ls.squareBody2.angle * -1;
 			
 			if(ls.life <= 0){
-				ls.squareBody2.position[0] = 960;
+                ls.squareBody2.position[0] = 960 + (Math.cos(inc) * 160);
 				ls.squareBody2.position[1] = 0;
 				ls.life = (Math.random() * 100) + 50;
 			}
- 
         }
-		let yCosine = Math.cos(inc) * 10;
-		planeBody.position[1] += yCosine;
-        plane.y = planeBody.position[1] *-1;
-
+		let yCosine = Math.cos(inc) * 4;
+		// planeBody.position[1] += yCosine;
+        // plane.y = planeBody.position[1] *-1;
+        
         // Move physics bodies forward in time
         world.step(30/60);
  
