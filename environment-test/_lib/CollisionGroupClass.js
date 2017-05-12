186495cf62a38d0946139e8b6494ea44fd79202a
spssciro <afsciro@spscommerce.com>
2017-05-12 08:17:14 -0400

    Adding barebones collision class

---
+++ environment-test/_lib/CollisionGroupClass.js
@@ -0,0 +1,12 @@
+class ControlClass {
+    constructor(){
+        // Player collision group
+        this.player = 1;
+        // World collision group
+        this.world = 2;
+        // Enemy collision group
+        this.enemy = 3;
+    }
+}
+
+export default ControlClass;
