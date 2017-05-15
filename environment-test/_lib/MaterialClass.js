aa92384baf3560a0bfd920c3cff132099f1c9935
spssciro <afsciro@spscommerce.com>
2017-05-15 07:56:44 -0400

    Stubbed material class

---
+++ environment-test/_lib/MaterialClass.js
@@ -0,0 +1,15 @@
+import WorldClass from './WorldClass';
+
+class MaterialClass {
+    constructor(x, y, w, h){
+    }
+    createMaterial(){
+        
+    }    
+    createContactMaterial(){
+        
+    }
+    createMaterialGroup(){
+
+    }
+}
